9d57df251b40f385c35d8903fe6f5ddba0e23988
Isobel Redelmeier <iredelmeier@gmail.com>
2019-03-23 16:34:04 -0700

    Refactor internals of Template

--- src/templates.rs
+++ src/templates.rs
@@ -3,16 +3,16 @@ use std::collections::HashMap;
 use crate::template::Template;
 
 lazy_static! {
-    pub static ref BASIC: Template<'static> = {
-        let html = include_bytes!("../templates/basic/resume.pug");
-        let stylesheet = include_bytes!("../templates/basic/style.scss");
+    pub static ref BASIC: Template = {
+        let html = include_bytes!("../templates/basic/resume.pug").to_vec();
+        let stylesheet = include_bytes!("../templates/basic/style.scss").to_vec();
 
         Template::new(html, stylesheet)
     };
 }
 
 lazy_static! {
-    pub static ref TEMPLATES: HashMap<&'static str, &'static Template<'static>> = {
+    pub static ref TEMPLATES: HashMap<&'static str, &'static Template> = {
         let mut templates = HashMap::new();
 
         templates.insert("basic", &*BASIC);
