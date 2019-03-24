42f08c535beaede4f64ee8196b76f8d3887c9d66
Isobel Redelmeier <iredelmeier@gmail.com>
2019-03-24 16:14:44 -0700

    Skip directory structure when archiving releases

--- scripts/release.sh
+++ scripts/release.sh
@@ -15,5 +15,5 @@ for target in "${TARGETS[@]}"; do
   rustup target add "$target"
   cargo build --release --target "$target"
   # cross build --release --target "$target"
-  tar czf  "/tmp/artifacts/resufancy-${target}.tar.gz" "target/${target}/release/resufancy"
+  tar czf  "/tmp/artifacts/resufancy-${target}.tar.gz" --directory "target/${target}/release" resufancy
 done
