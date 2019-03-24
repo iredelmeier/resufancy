bfe9afd3a0c00d5ab729df179d6ee5586301ee1e
Isobel Redelmeier <iredelmeier@gmail.com>
2019-03-24 15:46:16 -0700

    Only compile for linux
    :(

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,14 +2,18 @@
 
 set -eu
 
+# TODO: get cross working for cross-compilation
+
 TARGETS=(
   x86_64-unknown-linux-gnu
-  x86_64-apple-darwin
+  # x86_64-apple-darwin
 )
+
 mkdir -p /tmp/artifacts
 
 for target in "${TARGETS[@]}"; do
   rustup target add "$target"
-  cross build --release --target "$target"
+  cargo build --release --target "$target"
+  # cross build --release --target "$target"
   tar czf  "/tmp/artifacts/resufancy-${target}.tar.gz" "target/${target}/release/resufancy"
 done
