1cc96781d235bcbc68dbe6f1090f334a209d92f6
Isobel Redelmeier <iredelmeier@gmail.com>
2019-03-23 17:22:22 -0700

    Install wkhtmltopdf in CI

---
+++ scripts/install-wkhtmltox.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir -p /tmp
+cd /tmp || exit 1
+curl -L https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.3/wkhtmltox-0.12.3_linux-generic-amd64.tar.xz | tar xvJ
+cp wkhtmltox/lib/*.so* /usr/lib
