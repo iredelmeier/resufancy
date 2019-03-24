df3945ac5ff6a8ff869e3953be5f20536404dfd9
Isobel Redelmeier <iredelmeier@gmail.com>
2019-03-24 15:07:11 -0700

    Make install-wkhtmltox.sh fail better

--- scripts/install-wkhtmltox.sh
+++ scripts/install-wkhtmltox.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -eu
+
 mkdir -p /tmp
 cd /tmp || exit 1
 curl -L https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.3/wkhtmltox-0.12.3_linux-generic-amd64.tar.xz | tar xvJ
