7a3a4147a1c3bf3b4499707fddca781a082d8f60
Isobel Redelmeier <iredelmeier@gmail.com>
2019-03-23 18:19:43 -0700

    Remove Initializer and (unused) Loader

--- src/lib.rs
+++ src/lib.rs
@@ -11,18 +11,7 @@ pub mod template;
 pub mod templates;
 
 mod html_compiler;
-mod initializer;
 mod pdf_compiler;
 
-use filesystem::OsFileSystem;
-
 pub use crate::html_compiler::HtmlCompiler;
-pub use crate::initializer::Initializer;
 pub use crate::pdf_compiler::PdfCompiler;
-
-const HTML_PATH: &str = "resume.pug";
-const CSS_PATH: &str = "style.scss";
-
-lazy_static! {
-    static ref FILE_SYSTEM: OsFileSystem = OsFileSystem::new();
-}
