aaeceaa93fc8fd5be56533a9c5fbe5e3468c2a44
Isobel Redelmeier <iredelmeier@gmail.com>
2019-03-23 17:12:01 -0700

    Add PDF compilation
    [Closes #6]

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ extern crate filesystem;
 extern crate lazy_static;
 extern crate pug;
 extern crate sass_rs;
+extern crate wkhtmltopdf;
 
 pub mod error;
 pub mod resume;
@@ -11,11 +12,13 @@ pub mod templates;
 
 mod html_compiler;
 mod initializer;
+mod pdf_compiler;
 
 use filesystem::OsFileSystem;
 
 pub use crate::html_compiler::HtmlCompiler;
 pub use crate::initializer::Initializer;
+pub use crate::pdf_compiler::PdfCompiler;
 
 const HTML_PATH: &str = "resume.pug";
 const CSS_PATH: &str = "style.scss";
---
+++ src/pdf_compiler.rs
@@ -0,0 +1,43 @@
+use std::io::Read;
+
+use wkhtmltopdf::PdfApplication;
+
+use crate::error::Result;
+use crate::resume::Resume;
+
+#[derive(Debug, Clone, Default)]
+pub struct PdfCompiler;
+
+impl PdfCompiler {
+    pub fn new() -> Self {
+        Self
+    }
+
+    pub fn compile(&self, resume: &Resume) -> Result<Vec<u8>> {
+        let mut app = PdfApplication::new()?;
+        let mut builder = app.builder();
+        let mut pdf = builder.build_from_html(&resume.html())?;
+        let mut bytes: Vec<u8> = vec![];
+
+        pdf.read_to_end(&mut bytes)?;
+
+        Ok(bytes)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::templates::BASIC;
+    use crate::HtmlCompiler;
+
+    use super::*;
+
+    #[test]
+    fn compile_creates_pdf_from_valid_resume() {
+        let resume = HtmlCompiler::default().compile(&BASIC).unwrap();
+        let compiler = PdfCompiler::default();
+
+        let pdf = compiler.compile(&resume);
+        assert!(pdf.is_ok());
+    }
+}
